c87901b370ea090de2910f196c6cb850421feffa
OH-JOO-YEONG <brb1111@naver.com>
2021-02-24 01:56:05 +0900

    Feat: Github OAuth 로그인 with passport OAuth란? 인터넷 사용자들이 비밀번호를 제공하지 않고 다른 웹사이트 상의 자신들의 정보에 대해 웹사이트나 애플리케이션의 접근 권한을 부여할 수 있는 공통적인 수단으로서 사용되는, 접근 위임을 위한 개방형 표준이다.
    Flow
    1. Client(유저가 사용하는 서비스)가 Auth Server(Github) 로부터 Client ID, Client Secret, Redirect URL 모두 등록/발급 상태여야 한다.
    2. Resource Owner(유저)가 Auth Server(Github)에게 승인 요청을 보낸다.
    3. Resource Owner가 Client에게 Authorization Code를 보낸다.
    4. Client는 Client ID, Client Secret, Redirect URL, Authorization Code 네 가지를 모두 갖고 있는 상태이다.
    
    5. 위 네 가지 정보를 Auth Server에 보낸다. (Redirect URL은 optional)
    - redirect_url을 보내는 건 선택이다.
    - 하지만 Client ID, Client Secret, Authorization Code 세 가지는 꼭 보내야 한다.
    6. Resource Server는 네 가지 정보가 모두 일치하는지 확인한다.
    7. 하나라도 불일치 할 경우 OAuth 로그인은 실패한다.
    8. 모두 일치하면 Access Token을 발급한다.
    - Authorization Code 값은 더 이상 필요없기 때문에 삭제한다.
    
    부가적인 개념
     - Access Token 발급 -
    
    - 발급 받은 Access Token을 통해서 Resource Server의 기능을 이용할 수 있다.
    
    - Refresh Token -
    
    -Access Token은 cookie처럼 만료 기한이 있다.
    -따라서 만료가 되면 재발급 받아야한다.
    -이 때 사용하는 게 Refresh Token 이다.
    -서비스마다 Refresh Token을 발급하지 않는 경우도 있고 쓰는 방식도 다르다.
    -Access Token은 주로 세션에 저장한다.
    -만료 기한이 짧다.
    -Refresh Token은 중요하기 때문에 DB에 저장한다.
    -만료 기한이 길다
    
    Resolves #25

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,8 +33,38 @@ export const postLogin = passport.authenticate("local", {
     failureRedirect: routes.login,
 });
 
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+    const {
+        _json: { id, avata_url, name },
+    } = profile;
+    const { value: email } = profile.emails.filter((item) => item.primary)[0];
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            user.githubId = id;
+            user.save();
+            return cb(null, user);
+        }
+        const newUser = await User.create({
+            email,
+            name,
+            githubId: id,
+            avataUrl: avata_url,
+        });
+        return cb(null, newUser);
+    } catch (error) {
+        return cb(error);
+    }
+};
+
+export const postGithubLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
 export const logout = (req, res) => {
-    //
+    req.logout();
     res.redirect(routes.home);
 };
 
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,8 +1,17 @@
 import express from "express";
 import routes from "../routes";
+import passport from "passport";
 import { home, search } from "../controllers/videoController";
-import { getLogin, logout, getJoin, postJoin, postLogin } from "../controllers/userController";
-import { onlyPublic } from "../middleware";
+import {
+    getLogin,
+    logout,
+    getJoin,
+    postJoin,
+    postLogin,
+    githubLogin,
+    postGithubLogin,
+} from "../controllers/userController";
+import { onlyPrivate, onlyPublic } from "../middleware";
 
 const globalRouter = express.Router();
 
@@ -14,6 +23,14 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+    routes.githubCallback,
+    passport.authenticate("github", { failureRedirect: "/login" }),
+    postGithubLogin
+);
 
 export default globalRouter;
