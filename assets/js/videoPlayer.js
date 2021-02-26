2a6e2a47c4d522874051b7c8dcc84ec3a0a5631d
OH-JOO-YEONG <brb1111@naver.com>
2021-02-27 00:42:14 +0900

    Feat: 전체화면 - 화면 나가기 기능, 현재시간 - 전체시간보기 기능 1. Screen의 전체화면, 화면나가기 기능 추가 - jsFullscreen id값 할당 - getElementById로 id에 해당하는 태그를 받음. - 해당 태그가 커진지 안커진지 확인하는 메서드가 없어서 screenBool이라는 변수를 만듬. - 태그에다 addEventListener를 click 이벤트를 넣은 후 goAndExitFullScreen 함수를 만듬. - 조건문으로 screenBool을 이용한 후 requestFullscreen, exitFullscreen메서드를 사용해  해당 태그 값들을 수정하게 함.
    2. 비디오의 현재시간 - 전체시간 볼 수 있게함.
    - currentTime, totalTime의 id값을 각각 할당함.
    - 해당하는 태그를 받고, videoPlayer에 이벤트리스너 loadedmetadata, timeupdate일때 이벤트를 각각 추가함.
    - formatDate함수의 기능은 seconds의 값을 받으면 스트링으로 값을 리턴해줌.
    - setTotalTime, setCurrentTime 으로 각각 현재시간 전체시간 값을 받아옴.
    
    Resolves #32

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,8 +2,13 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScreenBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
-function handlePlayClick() {
+let screenBool = true;
+
+const handlePlayClick = () => {
     if (videoPlayer.paused) {
         videoPlayer.play();
         playBtn.innerHTML = '<i class="fas fa-pause"></i>';
@@ -11,9 +16,9 @@ function handlePlayClick() {
         videoPlayer.pause();
         playBtn.innerHTML = '<i class="fas fa-play"></i>';
     }
-}
+};
 
-function handleVolumeClick() {
+const handleVolumeClick = () => {
     if (videoPlayer.muted) {
         videoPlayer.muted = false;
         volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
@@ -21,11 +26,69 @@ function handleVolumeClick() {
         videoPlayer.muted = true;
         volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
     }
-}
+};
+
+const goAndExitFullScreen = () => {
+    if (screenBool) {
+        if (videoContainer.requestFullscreen) {
+            videoContainer.requestFullscreen();
+        } else if (videoContainer.mozRequestFullScreen) {
+            videoContainer.mozRequestFullScreen();
+        } else if (videoContainer.webkitRequestFullscreen) {
+            videoContainer.webkitRequestFullscreen();
+        } else if (videoContainer.msRequestFullscreen) {
+            videoContainer.msRequestFullscreen();
+        }
+        fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+        screenBool = !screenBool;
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+        fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+        screenBool = !screenBool;
+    }
+};
+
+const formatDate = (seconds) => {
+    const secondsNumber = parseInt(seconds, 10);
+    let hours = Math.floor(secondsNumber / 3600);
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+    let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (totalSeconds < 10) {
+        totalSeconds = `0${totalSeconds}`;
+    }
+    return ` ${hours}:${minutes}:${totalSeconds}`;
+};
+
+const setTotalTime = () => {
+    const totalTimeString = formatDate(videoPlayer.duration);
+    totalTime.innerHTML = totalTimeString;
+};
+
+const setCurrentTime = () => {
+    currentTime.innerHTML = `${formatDate(videoPlayer.currentTime)} `;
+};
 
 function init() {
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScreenBtn.addEventListener("click", goAndExitFullScreen);
+    videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+    videoPlayer.addEventListener("timeupdate", setCurrentTime);
 }
 
 if (videoContainer) {
